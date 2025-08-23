81444323d48ade705b2496fcb3b770f72d2bbcb1
김재원 <dumdum7@hs.ac.kr>
2025-08-23 20:33:16 +0900

    - Fix: foodList가 비어있을 경우 에러 메시지 출력하도록 변경

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -52,6 +52,13 @@ public class Main {
      */
     public static void addOrder(Store store, List<Food> foodList) {
 
+        if (foodList.isEmpty()) {
+
+            System.out.println("에러: 메뉴를 적어도 하나 선택해주세요.");
+            return;
+
+        }
+
         foodList.forEach(food -> {
             if (!store.getMenuList().contains(food)) {
                 System.out.println("에러: 유효한 주문이 아닙니다. (주문한 메뉴 미존재");
